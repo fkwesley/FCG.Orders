7b49eb92550843b04a16702216b10a4c89080cf2
fkwesley <frank-wesley95@hotmail.com>
2025-10-06 05:47:11 -0300

    refatoração regras de cartão

--- Domain/Entities/Order.cs
+++ Domain/Entities/Order.cs
@@ -2,6 +2,7 @@
 using Domain.Exceptions;
 using Domain.ValueObjects;
 using System.Diagnostics;
+using System.Globalization;
 
 namespace Domain.Entities
 {
@@ -49,9 +50,37 @@ namespace Domain.Entities
             { 
                 if (PaymentMethod != PaymentMethod.Pix && value == null)
                     throw new BusinessException("Payment method details are required for credit or debit card payments.");
+                
+                if (value != null)
+                {
+                    if (!IsValidCardNumber(value.Value.CardNumber))
+                        throw new BusinessException("Invalid card number.");
+                    
+                    if (!IsValidExpiryDate(value.Value.ExpiryDate))
+                        throw new BusinessException("The card has already expired or is invalid. Provide a new card");
+                }
 
                 _paymentMethodDetails = value;
             }
+        }
+
+
+        public bool IsValidCardNumber(string cardNumber)
+        {
+            return !string.IsNullOrWhiteSpace(cardNumber) && cardNumber.Length >= 13 && cardNumber.Length <= 19;
+        }
+
+        public bool IsValidExpiryDate(string expiryDate)
+        {
+            //returns true if the expiry date is in the future
+            if (DateTime.TryParseExact(expiryDate, "yyyy-MM", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate))
+            {
+                // Considera o último dia do mês como válido
+                var lastDayOfMonth = new DateTime(parsedDate.Year, parsedDate.Month, DateTime.DaysInMonth(parsedDate.Year, parsedDate.Month));
+                return lastDayOfMonth >= DateTime.UtcNow.Date;
+            }
+            else
+                return false;
         }
 
     }
